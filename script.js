f4043ac96f0e7273b3fa673b7802ce4a9ef08c66
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-12 02:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
-
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let articleLessArray = [];
+let mp = {};
+let regexp = /\bthe\b|\ban\|\ba\b/gi;
+for(let i=0; i<arr.length; i=i+1)
+	{
+		let temp = arr[i].replace(regexp, "").trim();
+		articleLessArray.push(temp);
+		mp[temp] = arr[i];
+	}
+// console.log(articleLessArray);
+articleLessArray.sort();
+// console.log(articleLessArray);
+let ans = [];
+for(let i of articleLessArray)
+	{
+		ans = push(mp[i]);
+	}
+console.log(ans);
