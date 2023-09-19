b506c163ee40a8972a7e63b1fc5b99d6ad69aadc
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-19 16:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //your code here
-let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let articleLessArray = [];
-let mp = {};
-let regexp = /\bthe\b|\ban\|\ba\b/gi;
-for(let i=0; i<arr.length; i=i+1)
-	{
-		let temp = arr[i].replace(regexp, "").trim();
-		temp = temp.replace(/  /g, " ");
-		articleLessArray.push(temp);
-		mp[temp] = arr[i];
-	}
-// console.log(articleLessArray);
-articleLessArray.sort();
-// console.log(articleLessArray);
-let ans = [];
-for(let i of articleLessArray)
-	{
-		ans.push(mp[i]);
-	}
-console.log(ans);
+
+let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Rolling Stones', 'The Who'];
+
+// remove articles from band names
+const removeArticle = (name) => {
+  return name.replace(/^(a|an|the)\s+/i, '').trim();
+};
+bandNames = bandNames.map(removeArticle);
+
+// sort band names in lexicographic order
+bandNames.sort((a, b) => a.localeCompare(b));
+
+// display band names in an unordered list
+const bandList = document.getElementById('band');
+bandNames.forEach((name) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = name;
+  bandList.appendChild(listItem);
+});
